20cbd5a63627abb83b04d5b7e24876f1041d2824
AhnEunJin <122256847+SophieMarieSo@users.noreply.github.com>
2024-10-04 18:56:20 +0900

    Title: 입력받아 연산하기, Time: 194ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241004/입력받아 연산하기/take-input-and-operate.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split('\n');
+
+const a = Number(input[0]) + 87
+const b = Number(input[1]) % 10
+
+console.log(a)
+console.log(b)
