261ce5f8431387f0960e96235b23832f1f4813b5
AnEunJin <122256847+SophieMarieSo@users.noreply.github.com>
2024-11-15 18:22:12 +0900

    Title: 시험 통과 여부 확인하기, Time: 171ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241115/시험 통과 여부 확인하기/verify-test-passed.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString();
+const score = Number(input);
+
+if (score >= 80) {
+    console.log('pass')
+}
+else {
+    console.log(`${80-score} more score`)
+}
